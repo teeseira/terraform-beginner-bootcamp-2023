43a6a137615f0d260c7e89907986fdc6ed98ef56
Tidi M. <tidi.matts@gmail.com>
2023-10-03 17:32:13 +0000

    #31 only upload index.html if a content version has changed

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -28,6 +28,10 @@ resource "aws_s3_object" "index_html" {
   content_type = "text/html"
 
   etag = filemd5(var.index_html_filepath)
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
+  }
 }
 
 resource "aws_s3_object" "error_html" {
@@ -37,6 +41,10 @@ resource "aws_s3_object" "error_html" {
   content_type = "text/html"
   
   etag = filemd5(var.error_html_filepath)
+  #lifecycle {
+  #  replace_triggered_by = [terraform_data.content_version.output]
+  #  ignore_changes = [etag]
+  #}
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
@@ -60,4 +68,8 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       }
     }
   })
+}
+
+resource "terraform_data" "content_version" {
+  input = var.content_version
 }
