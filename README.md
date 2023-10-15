7c13e2668c3244606f00873f6f9e482196a91bef
Tidi-Seira <72232914+teeseira@users.noreply.github.com>
2023-10-15 01:28:07 +0100

    #49 fix issues with providers, setup two different towns. (#50)

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Terraform Beginner Bootcamp 2023
+
+## Project Overview
+TerraTowns is like a central hub for Terraform enthusiasts to share and connect their personal websites. These websites are hosted on their own servers and are called TerraHouse. Terraform enthusiasts use Infrastructure as Code (IaC) to deploy their TerraHouse, which is like a tool that lets them build their websites and seamlessly integrate them into the TerraTowns community.
+
+## TerraTowns Architectural Diagram 
 ![terraform bootcamp](https://github.com/teeseira/terraform-beginner-bootcamp-2023/assets/72232914/4b6d09fc-7f76-403f-9313-dce81bf6d65e)
 
 ## Weekly Journals
--- main.tf
+++ main.tf
@@ -5,27 +5,34 @@ terraform {
       version = "1.0.0"
     }
   }
-#  cloud {
-#    organization = "tm2entity"
-#
-#    workspaces {
-#      name = "terra-house-1"
-#    }
-#  }
+  cloud {
+    organization = "tm2entity"
+
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
 }
+
+
+
+
+
+
+
+
+
 provider "terratowns" {
   endpoint = var.terratowns_endpoint
   user_uuid = var.teacherseat_user_uuid
   token = var.terratowns_access_token
 }
 
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
+module "home_avatar_hosting" {
+  source = "./modules/terrahome_aws"
   user_uuid = var.teacherseat_user_uuid
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  content_version = var.content_version
-  assets_path = var.assets_path
+  public_path = var.avatar.public_path
+  content_version = var.avatar.content_version
 }
 
 resource "terratowns_home" "home" {
@@ -33,7 +40,27 @@ resource "terratowns_home" "home" {
   description = <<DESCRIPTION
 "Avatar: The Game" is an action-adventure video game.  Set on the visually stunning world of Pandora, players can choose to play as human soldiers or Na'vi, each offering distinct gameplay experiences. The game combines exploration, interaction with the planet's wildlife, and thrilling battles, providing an opportunity to delve into the rich world of Pandora while enjoying an original storyline that complements the movie's narrative.
 DESCRIPTION
-  domain_name = module.terrahouse_aws.cloudfront_url
-  town = "missingo"
-  content_version = 1
+  domain_name = module.home_avatar_hosting.domain_name
+  town = "gamers-grotto"
+  content_version = var.avatar.content_version
+}
+
+
+
+
+module "home_success_hosting" {
+  source = "./modules/terrahome_aws"
+  user_uuid = var.teacherseat_user_uuid
+  public_path = var.success.public_path
+  content_version = var.success.content_version
+}
+
+resource "terratowns_home" "home_success" {
+  name = "Recipe for Success"
+  description = <<DESCRIPTION
+"Regardless of how you personally define a successful life, it's your continuous self-improvement that gives purpose to your accomplishments."
+DESCRIPTION
+  domain_name = module.home_success_hosting.domain_name
+  town = "cooker-cove"
+  content_version = var.success.content_version
 }
